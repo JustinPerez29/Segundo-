3be7ede360e6421e741c890667903a98cb98f681
JustinPerez29 <68259792+JustinPerez29@users.noreply.github.com>
2020-07-28 22:00:10 -0500

    Update Primario.java

--- space/Primario.java
+++ space/Primario.java
@@ -1,6 +1,7 @@
 package space;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 
 public class Primario {
 	
@@ -9,6 +10,12 @@ private ArrayList<User> list;
 public Primario() {
 		
 	list = new ArrayList<User>();
+	list.add(new User( 1,"Antonio", "Preciado", 1400000.1));
+        list.add(new User(2, "Bruno", "Casas", 1500000.1));
+        list.add(new User(10,"Alvaro", "Apita",  700000.1));
+        list.add(new User(4,"Alejandro", "Torres",  400000.2));
+        list.add(new User(11,"Geraldine", "Gomèz",  600000.2));
+        list.add(new User(15,"Jesus", "Fonseca",  500000.6));
 
 	}
 	
@@ -23,17 +30,17 @@ public User buscarEmpleado( int id ) {
 				return list.get(i);
 			}
 		}
-		return null;
+	return null;
 	}
 	
 public boolean agregar(int id, String nombre, String apellido, double salario) throws Exception {
 	if (buscarEmpleado(id) != null) {
-		throw new Exception("No se puede agregar el id ya existe");
+	throw new Exception("Imposible agregarlo, id existente");
 		}
 	else {
-	User nuevo = new User (id, nombre, apellido, salario); 
-	    list.add(nuevo);
-	    return true; 
+	User nuevo = new User(id, nombre, apellido, salario); 
+	list.add(nuevo);
+	return true;
 		}
 	}
 	
@@ -51,27 +58,79 @@ public boolean eliminar( int id) throws Exception{
 	}
 	
 	
-	public boolean modificar(int id, String nombre, String apellido, double salario)throws Exception {
+public boolean modificar(int id, String nombre, String apellido, double salario)throws Exception {
 		
-		User amodificar = buscarEmpleado(id);
-		if (amodificar != null) {
-			amodificar.setNombre(nombre);
-			amodificar.setApellido(apellido);
-			amodificar.setSalario(salario);
-			return true;
+	User amodificar = buscarEmpleado(id);
+	if (amodificar != null) {
+	amodificar.setNombre(nombre);
+	amodificar.setApellido(apellido);
+	amodificar.setSalario(salario);
+	return true;
 		}
-		else {
-			throw new Exception("No existe, no se puede continuar");
+	else {
+	throw new Exception("No existe,imposible continuar");
 		}
 		
 	}
 	
+public String empleadoMayorSalar() {
+	return list.stream()
+        .max(Comparator.comparing(User::getSalario))
+        .map(User::toString)
+        .orElse("Sin empleados");
+	}
 	
-	public String mostrar() {
-		String rest  = "";
-		for (int i = 0; i < list.size(); i++) {
-			rest += list.get(i).toString() + "\n";
-		}
+public String empleadoMenorSalar() {
+	return list.stream()
+        .min(Comparator.comparing(User::getSalario))
+        .map(User::toString)
+        .orElse("Sin empleados");
+	}
+	
+public String ordenarNombre() {
+         String rest = "";
+	  rest = list.stream()
+        .sorted(Comparator.comparing(User::getNombre))
+        .map(User::toString)
+        .reduce(" ", (a,b) -> {
+        	return a+"\n"+b; 
+        });
+		
 		return rest;
+	}
+	
+public long mostrarTotalDeNombresPorletrA() {
+	return list.stream()
+        .filter(empleado -> empleado.getApellido().toUpperCase().startsWith("A"))
+        .count();
+		
+	}
+	
+public String cincoEmpleadosMayorSalar() {
+	 return list.stream()
+        .sorted(Comparator.comparing(User::getSalario).reversed())
+        .map(User::toString)
+        .limit(5)
+        .reduce(" ", (a,b) -> {
+         return a+"\n"+b; 
+                });
+	}
+	
+public double sumaSalariosMayorSete() {
+	return list.stream().filter((list) -> {
+	return list.getSalario() > 700000;
+		})
+	.map(User::getSalario)
+	.reduce(0.0, (a,b) ->{
+	return a+b;
+		});
+	}
+	
+public String mostrar() {
+	String rest  = "";
+	for (int i = 0; i < list.size(); i++) {
+	rest += list.get(i).toString() + "\n";
+		}
+	return rest;
 	}
 }
